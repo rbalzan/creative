fcae6da9f1b370e0eca014f96cbcc9b0feb7ddde
rbalzan <30488513+rbalzan@users.noreply.github.com>
2017-07-27 01:50:42 -0400

    Traduccion al ingles

--- application/langs/en/auth.php
+++ application/langs/en/auth.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'email_required' => '¡Necesitamos saber tu dirección de correo electrónico!',
-    'pass_required' => 'Necesitas ingresar tu contraseña',
-    'failed' => 'La combinación de dirección de correo electrónico y contraseña no es correcta',
-    'throttle'=> 'Demasiados intentos de inicio de sesión. Inténtalo de nuevo en unos segundos.',
-    'inactive'=> 'Tu cuenta ha sido bloqueada.',
-    'activate'=> 'Debe confirmar tu registro. Hemos enviado un Correo Electrónico con los pasos para activar su cuenta.',
-    'welcome'=> 'Bienvenido(a).'
+    'email_required' => 'You need to know your email address!',
+    'pass_required' => 'You need to enter your password.',
+    'failed' => 'The combination of email address and password is not correct.',
+    'throttle'=> 'Too many login attempts. Please try again in a few seconds.',
+    'inactive'=> 'Your account has been blocked.',
+    'activate'=> 'You must confirm your registration. We have sent an Email with the steps to activate your account.',
+    'welcome'=> 'Welcome!'
 ];
 
 ?>
