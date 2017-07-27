1eec92b6f9c072df4ce88b07089cdcc894521ab9
Brayan Rincon <brayan262@gmail.com>
2017-07-27 01:10:24 -0430

    Languaje English

---
+++ application/langs/en/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'email_required' => 'We need to know your email address!',
+    'pass_required' => 'Necesitas ingresar tu contraseña',
+    'failed' => 'La combinación de dirección de correo electrónico y contraseña no es correcta',
+    'throttle'=> 'Demasiados intentos de inicio de sesión. Inténtalo de nuevo en unos segundos.',
+    'inactive'=> 'Tu cuenta ha sido bloqueada.'
+];
+
+?>
