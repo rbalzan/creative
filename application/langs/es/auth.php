cd0ca6aeff3526b6a85998167b744047f85338cd
Brayan Rincon <brayan262@gmail.com>
2017-07-27 01:10:48 -0430

    Languaje Spanish

--- application/langs/es/auth.php
+++ application/langs/es/auth.php
@@ -5,7 +5,9 @@ return [
     'pass_required' => 'Necesitas ingresar tu contraseña',
     'failed' => 'La combinación de dirección de correo electrónico y contraseña no es correcta',
     'throttle'=> 'Demasiados intentos de inicio de sesión. Inténtalo de nuevo en unos segundos.',
-    'inactive'=> 'Tu cuenta ha sido bloqueada.'
+    'inactive'=> 'Tu cuenta ha sido bloqueada.',
+    'activate'=> 'Debe confirmar tu registro. Hemos enviado un Correo Electrónico con los pasos para activar su cuenta.',
+    'welcome'=> 'Bienvenido(a).'
 ];
 
 ?>
