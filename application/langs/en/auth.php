9966d6b2d8cd0b63dcffffb383ef2a2c0022f6f6
Brayan Rincon <brayan262@gmail.com>
2017-07-27 01:14:21 -0430

    Append keys

--- application/langs/en/auth.php
+++ application/langs/en/auth.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'email_required' => 'We need to know your email address!',
+    'email_required' => '¡Necesitamos saber tu dirección de correo electrónico!',
     'pass_required' => 'Necesitas ingresar tu contraseña',
     'failed' => 'La combinación de dirección de correo electrónico y contraseña no es correcta',
     'throttle'=> 'Demasiados intentos de inicio de sesión. Inténtalo de nuevo en unos segundos.',
-    'inactive'=> 'Tu cuenta ha sido bloqueada.'
+    'inactive'=> 'Tu cuenta ha sido bloqueada.',
+    'activate'=> 'Debe confirmar tu registro. Hemos enviado un Correo Electrónico con los pasos para activar su cuenta.',
+    'welcome'=> 'Bienvenido(a).'
 ];
 
 ?>
